2d7e68dc0c957fc63a3f20849db0ee23dd5c538d
yang-jian-bing <you@example.com>
2023-02-04 10:54:39 +0800

    first commit

---
+++ pages/API/login/login.vue
@@ -0,0 +1,391 @@
+<template>
+  <view class="login-register">
+    <view class="con">
+      <view class="logo-box">
+        <image
+          src="@/assets/img/login/min-logo.jpg"
+          class="min-logo"
+          alt=""
+          srcset=""
+        />
+      </view>
+      <view class="loginTitle">橄榄海</view>
+      <!-- 验证码登录 -->
+      <view class="item-box">
+        <view class="item">
+          <input
+            v-model="principal"
+            type="text"
+            class="text"
+            placeholder="请输入手机号"
+          />
+        </view>
+        <view v-if="errorTips == 3" class="error"
+          ><text class="error-icon">!</text>请输入手机号</view
+        >
+        <view v-if="errorTips == 4" class="error"
+          ><text class="error-icon">!</text>请输入正确的手机号</view
+        >
+        <view class="item">
+          <input
+            v-model="captchaVerification"
+            type="text"
+            class="text"
+            placeholder="请输入验证码"
+          />
+          <view class="get-captcha" @click="getYzm">{{ text }}</view>
+        </view>
+        <view v-if="errorTips == 5" class="error"
+          ><text class="error-icon">!</text>请输入验证码</view
+        >
+      </view>
+      <view class="btn" @click="login">登录</view>
+    </view>
+  </view>
+</template>
+
+<script>
+import { getAction, postAction } from "../../../utils/manage.js";
+import crypto from "crypto";
+export default {
+  data() {
+    return {
+      isAccountLogin: true, // 账号登录
+      captchaVerification: "", // 验证码
+      credentials: "", // 密码
+      text: "获取验证码",
+      num: 59,
+      principal: "", // 用户名/邮箱/手机号
+      sysType: 0, // 系统类型 0.普通用户系统 1.商家端
+      errorTips: 0, // 输入错误提示: 0无  1账号有误或为空 2密码输入错误或为空 3手机号为空 4手机号输入错误 5验证码输入错误
+    };
+  },
+  methods: {
+    // 登录
+    login() {
+      if (this.principal.trim() == "") {
+        uni.showToast({
+          title: "请输入手机号",
+          icon: "error",
+        });
+        return false;
+      }
+      if (this.captchaVerification.trim() == "") {
+        uni.showToast({
+          title: "请输入验证码",
+          icon: "error",
+        });
+        return false;
+      }
+
+      const params = {
+        phoneNum: this.principal,
+        verifCode: this.captchaVerification,
+        checkSum: "starlab",
+      };
+
+      postAction("/rest/user/login?checkSum=starlab", params).then(
+        (res) => {
+          Object.keys(res).map((key) => {
+            uni.setStorageSync(key, res[key]);
+          });
+          uni.showToast({
+            title: "登录成功",
+            icon: "success",
+          });
+          uni.reLaunch({
+            // 关闭所有页面，打开首页
+            url: "/pages/tabBar/home/home",
+          });
+        },
+        (err) => {
+          Toast.clear();
+          console.log(err);
+        }
+      );
+    },
+    getYzm() {
+      if (this.num != 59) {
+        return;
+      }
+      const phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/;
+      if (!phoneReg.test(this.principal)) {
+        uni.showToast({
+          title: "请输入正确手机号",
+          icon: "error",
+        });
+        return;
+      } else {
+        this.phoneError = false;
+      }
+
+      const condition = {
+        phoneNum: this.principal,
+        checkSum: "starlab",
+      };
+
+      var sha1 = crypto.createHash("sha1");
+      sha1.update(this.captchaVerification.split("").reverse().join(""));
+      const code_sha = sha1.digest("hex").substring(10, 20);
+      condition.sign = code_sha;
+
+      // request(params);
+      getAction("/rest/user/verif-code", condition)
+        .then(() => {
+          uni.showToast({
+            title: "发送成功",
+            icon: "success",
+          });
+
+          this.disabled = true;
+          const int = setInterval(() => {
+            this.text = this.num + "s";
+            this.num--;
+            if (this.num === 0) {
+              this.disabled = false;
+              clearInterval(int);
+              this.text = "发送验证码";
+              this.num = 59;
+            }
+          }, 1000);
+        })
+        .catch((err) => {
+          console.log(err);
+
+          Toast(err);
+          return;
+        });
+    },
+    // 去注册
+    toRegister() {
+      uni.redirectTo({
+        url: "/pages/register/register",
+      });
+    },
+
+    // 切换登录方式
+    changeLoginWay() {
+      this.isAccountLogin = !this.isAccountLogin;
+    },
+
+    // 忘记密码
+    forgetPassword() {
+      uni.navigateTo({
+        url: "/pages/forget-password/forget-password",
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.logo-box {
+  text-align: center;
+  padding-top: 100px;
+}
+.min-logo {
+  width: 68px;
+  height: 68px;
+}
+.loginTitle {
+  text-align: center;
+  height: 25px;
+  font-size: 18px;
+  margin-top: 10px;
+  font-family: PingFangSC-Medium, PingFang SC;
+  font-weight: bold;
+  color: #333333;
+  line-height: 25px;
+}
+.loginMinTitle {
+  height: 19px;
+  font-size: 14px;
+  font-family: Avenir-Book, Avenir;
+  font-weight: normal;
+  color: #333333;
+  line-height: 19px;
+  text-align: center;
+  margin-top: 5px;
+  margin-bottom: 23px;
+}
+.login-register {
+  padding-bottom: env(safe-area-inset-bottom);
+}
+
+.login-register .tab {
+  display: flex;
+  align-items: baseline;
+  padding: 160rpx 60rpx 80rpx;
+}
+
+.login-register .tab .item {
+  margin-right: 30rpx;
+  font-size: 32rpx;
+  font-weight: 600;
+  position: relative;
+}
+
+.login-register .tab .item::after {
+  position: absolute;
+  bottom: -16rpx;
+  left: 10%;
+  right: 10%;
+  display: block;
+  width: auto;
+  height: 6rpx;
+  border-radius: 6rpx;
+  content: " ";
+  font-size: 0;
+  background: #fff;
+}
+
+.login-register .tab .item.active {
+  font-size: 48rpx;
+}
+
+.login-register .tab .item.active::after {
+  background: linear-gradient(to right, #fc1b35, #fff);
+}
+
+.login-register .con {
+  padding: 0 60rpx;
+}
+
+.login-register .con .item {
+  margin-top: 30rpx;
+  padding: 30rpx 0;
+  border-bottom: 2rpx solid #fafafa;
+  display: flex;
+  align-items: center;
+}
+
+.login-register .con .item .text {
+  font-size: 30rpx;
+  flex: 1;
+  margin-right: 20rpx;
+}
+
+.login-register .con .item .get-captcha {
+  font-size: 28rpx;
+  padding: 0 20rpx;
+  color: #fc1b35;
+}
+
+.login-register .con .btn {
+  color: #fff;
+  background: #fc1b35;
+  height: 90rpx;
+  line-height: 90rpx;
+  margin-top: 70rpx;
+  border-radius: 120rpx;
+  text-align: center;
+  font-size: 32rpx;
+  font-weight: 600;
+}
+
+.login-register .con .flex-item {
+  margin-top: 30rpx;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20rpx;
+  color: #999;
+  font-size: 28rpx;
+}
+
+.login-register .another-login {
+  position: fixed;
+  bottom: 200rpx;
+  left: 60rpx;
+  right: 60rpx;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.login-register .another-login .text {
+  font-size: 28rpx;
+  color: #999;
+  margin-left: 20rpx;
+}
+
+.login-register .another-login .img-box {
+  display: flex;
+}
+
+.login-register .another-login .img-box .img {
+  width: 50rpx;
+  height: 50rpx;
+  font-size: 0;
+  padding: 0 20rpx;
+  overflow: hidden;
+}
+
+.login-register .con .consent-agreement {
+  margin-top: 30rpx;
+  padding: 0 20rpx;
+  color: #999;
+  font-size: 28rpx;
+}
+
+.login-register .con .consent-agreement .agreement {
+  color: #2a6de9;
+}
+
+.login-register .popup-con-middle.register-success {
+  padding: 40rpx;
+  left: 140rpx;
+  right: 140rpx;
+}
+
+.login-register .register-success .img {
+  position: relative;
+  width: 100rpx;
+  height: 100rpx;
+  margin: auto;
+  border-radius: 50%;
+  background: #fc1b35;
+}
+
+.login-register .register-success .img::after {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  display: block;
+  font-size: 28rpx;
+  font: normal normal normal 48rpx/1 uni;
+  color: #fff;
+  content: "\EA08";
+  transform: translateY(-50%);
+}
+
+.login-register .register-success .text {
+  font-size: 28rpx;
+  margin-top: 30rpx;
+  font-weight: 600;
+}
+
+.login-register .register-success .des {
+  color: #999;
+  margin-top: 20rpx;
+}
+
+/* 错误 */
+.login-register .error {
+  font-size: 12px;
+  color: #fc1b35;
+  line-height: 1.5em;
+}
+.login-register .error .error-icon {
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  line-height: 14px;
+  font-size: 12px;
+  border-radius: 50%;
+  background: #fc1b35;
+  color: #fff;
+  text-align: center;
+  margin-right: 5px;
+}
+</style>
---
+++ pages/tabBar/home/home.vue
@@ -0,0 +1,413 @@
+<template>
+  <view class="page">
+    <view class="header-box">
+      <view class="pageheaders">
+        <view @click="showPicker = true"
+          >{{ currentHouse.address }}</view>
+        <view>
+          <img
+            src="../../../assets/img/home-icon/sqtz.jpg"
+            class="img-100"
+            alt=""
+            srcset=""
+          />
+          <img
+            src="../../../assets/img/home-icon/dh.jpg"
+            class="img-100 m-l-15"
+            alt=""
+            srcset=""
+          />
+        </view>
+      </view>
+      <view>
+        <view class="home-search flex-between">
+          <view class="search-content">物业</view>
+          <view class="search-icon">搜索</view>
+        </view>
+        <!-- <van-search shape="round" placeholder="请输入搜索关键词" /> -->
+      </view>
+      <view class="flex m-t-10 p-b-20">
+        <view class="min-icon">
+          <view class="text-center">
+            <view class="icon iconfont">&#xe7f1;</view>
+          </view>
+          <p class="min-title">我的房屋</p>
+        </view>
+        <view class="min-icon">
+          <view class="text-center">
+            <view class="icon iconfont">&#xe8b1;</view>
+          </view>
+          <p class="min-title">物业服务</p>
+        </view>
+        <view class="min-icon" @click="goPage('paymentTypePage')">
+          <view class="text-center">
+            <view class="icon iconfont">&#xe88d;</view>
+          </view>
+          <p class="min-title">去缴费</p>
+        </view>
+        <view class="min-icon">
+          <view class="text-center">
+            <view class="icon iconfont">&#xfaf2;</view>
+            <!-- <img src="../../assets/img/home-icon/gzbx.jpg" class="img-40" alt="" srcset=""> -->
+          </view>
+          <p class="min-title">故障报修</p>
+        </view>
+      </view>
+    </view>
+    <view class="main-box">
+      <view class="module-icon" :key="index" v-for="(item, index) in moduleList">
+        <view class="module-icon-img">
+          <!-- <svg class="icon svg-icon" aria-hidden="true">
+            <use v-bind:xlink:href="item.icon"></use>
+          </svg> -->
+          <img :src="item.imgUrl" alt="" />
+        </view>
+        <p class="module-title">{{ item.name }}</p>
+      </view>
+    </view>
+    <!-- <van-popup v-model="showPicker" position="bottom">
+      <van-picker
+        required
+        show-toolbar
+        :columns="columns"
+        value-key="address"
+        @confirm="onConfirm"
+        @cancel="showPicker = false"
+      >
+      </van-picker>
+    </van-popup> -->
+  </view>
+</template>
+
+<script>
+import { getAction } from "../../../utils/manage.js";
+
+// import "../../../assets/font/iconfont.js";
+export default {
+  components: {},
+  data() {
+    return {
+      showPicker: false,
+      selectName: "",
+      selectId: "",
+      columns: [
+        {
+          text: "缤纷南郡2号楼三单元1234",
+          id: 12,
+        },
+        {
+          text: "缤纷南郡2号楼四单元1233",
+          id: 12,
+        },
+      ],
+      currentHouse: {},
+      index: 1,
+      newsList: [
+        {
+          title: "求助",
+          content: "本小区有没有可租的房子，联系电话：18787473843",
+        },
+        {
+          title: "车位出租",
+          content: "出租5号楼车位，价格面议，有意者联系18343434344",
+        },
+        {
+          title: "活动",
+          content: "双十一缴物业费送礼品",
+        },
+      ],
+      list: [],
+      moduleList: [
+        {
+          name: "社区通知",
+          id: "",
+          imgUrl: require("../../../assets/img/home/sqtz.png"),
+          icon: "#icon-gonggao",
+        },
+        {
+          name: "社区送菜",
+          id: "",
+          imgUrl: require("../../../assets/img/home/sqsc.png"),
+          icon: "#icon-sousuo",
+        },
+        {
+          name: "海鲜市场",
+          id: "",
+          imgUrl: require("../../../assets/img/home/hxsc.png"),
+          icon: "#icon-fushi",
+        },
+        {
+          name: "维修",
+          id: "",
+          imgUrl: require("../../../assets/img/home/wx.png"),
+          icon: "#icon-wuliu",
+        },
+        {
+          name: "干洗",
+          id: "",
+          imgUrl: require("../../../assets/img/home/gx.png"),
+          icon: "#icon-wuliu",
+        },
+        {
+          name: "代办汽车保养",
+          id: "",
+          imgUrl: require("../../../assets/img/home/dbqcby.png"),
+          icon: "#icon-jiaju",
+        },
+        {
+          name: "代办事故处理",
+          id: "",
+          imgUrl: require("../../../assets/img/home/dbsgcl.png"),
+          icon: "#icon-zengsong",
+        },
+        {
+          name: "代办验车",
+          id: "",
+          imgUrl: require("../../../assets/img/home/dbyc.png"),
+          icon: "#icon-zhanghaoguanli",
+        },
+        {
+          name: "陪护",
+          id: "",
+          imgUrl: require("../../../assets/img/home/ph.png"),
+          icon: "#icon-huodong",
+        },
+        {
+          name: "家政",
+          id: "",
+          imgUrl: require("../../../assets/img/home/jz.png"),
+          icon: "#icon-huodong",
+        },
+        {
+          name: "接送娃",
+          id: "",
+          imgUrl: require("../../../assets/img/home/jsw.png"),
+          icon: "#icon-jiaoyu",
+        },
+        {
+          name: "托儿服务",
+          id: "",
+          imgUrl: require("../../../assets/img/home/tefw.png"),
+          icon: "#icon-wupinshenqing",
+        },
+        {
+          name: "家教",
+          id: "",
+          imgUrl: require("../../../assets/img/home/jj.png"),
+          icon: "#icon-peisong",
+        },
+        {
+          name: "旧物",
+          id: "",
+          imgUrl: require("../../../assets/img/home/jw.png"),
+          icon: "#icon-qiche",
+        },
+        {
+          name: "论坛",
+          id: "",
+          imgUrl: require("../../../assets/img/home/lt.png"),
+          icon: "#icon-luntan",
+        },
+        {
+          name: "家装",
+          id: "",
+          imgUrl: require("../../../assets/img/home/jzfw.jpg"),
+          icon: "#icon-luntan",
+        },
+        {
+          name: "定制实木家具",
+          id: "",
+          imgUrl: require("../../../assets/img/home/dzsmjj.jpg"),
+          icon: "#icon-luntan",
+        },
+        {
+          name: "定制门窗",
+          id: "",
+          imgUrl: require("../../../assets/img/home/dzmc.jpg"),
+          icon: "#icon-luntan",
+        },
+      ],
+    };
+  },
+  created() {
+    this.getHouses();
+  },
+  methods: {
+    getHouses() {
+      getAction("/rest/user/houses?checkSum=starlab").then(
+        (res) => {
+          this.columns = res.houses;
+          this.currentHouse = this.columns[0];
+          uni.setStorageSync("currentHouse", JSON.stringify(this.columns[0]));
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+
+    goPage(page) {
+      this.$bridge.call(page);
+    },
+    onConfirm(value) {
+      this.selectName = value;
+      this.currentHouse = value;
+      uni.setStorageSync("currentHouse", JSON.stringify(value));
+      this.showPicker = false;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+// @import "../../../assets/font/iconfont.css";
+.home-search {
+  height: 40px;
+  line-height: 40px;
+  border: solid 2px rgb(221, 44, 52);
+  border-radius: 20px;
+  margin: 10px;
+  padding: 5px;
+  .search-content {
+    height: 26px;
+    line-height: 26px;
+    color: #999;
+    padding-left: 10px;
+  }
+  .search-icon {
+    height: 26px;
+    line-height: 26px;
+    background: rgb(221, 44, 52);
+    border-radius: 13px;
+    color: #fff;
+    width: 50px;
+    text-align: center;
+  }
+}
+.img-100 {
+  width: 20px;
+  height: 20px;
+}
+.img-40 {
+  width: 30px;
+  height: 30px;
+}
+.header-box {
+  //  background: #dd2c34;
+  //  color: #fff;
+}
+.van-field__left-icon {
+  background: rgb(221, 44, 52);
+  padding: 0px 10px;
+  border-radius: 12px;
+  color: #fff;
+}
+
+.van-search .van-cell {
+  padding: 3px;
+}
+
+.min-icon {
+  width: 25%;
+  .iconfont {
+    font-size: 40px;
+    color: #dd2c34;
+  }
+  .min-title {
+    text-align: center;
+    font-size: 13px;
+    font-weight: 500;
+  }
+}
+.svg-icon {
+  width: 40px;
+  height: 40px;
+}
+.banner {
+  display: block;
+  width: 100%;
+}
+
+.pageheaders {
+  width: 93%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  font-size: 14px;
+  font-weight: 600;
+}
+.main-box {
+  display: flex;
+  flex-wrap: wrap;
+  background: #fff;
+  margin: 10px;
+}
+.module-icon {
+  width: 20%;
+  text-align: center;
+  background: #fff;
+  padding-bottom: 20px;
+}
+.module-icon-img {
+  img {
+    width: 100%;
+  }
+}
+.module-title {
+  font-weight: bold;
+}
+.news-box {
+  margin-top: 15px;
+  padding: 5px;
+  background: #ddd;
+  .active {
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .arrow {
+    font-size: 20px;
+  }
+  .item-box {
+    background: #fff;
+    margin: 5px;
+    margin-bottom: 10px;
+    border-radius: 6px;
+    overflow: hidden;
+    img {
+      width: 100% !important;
+      height: 100% !important;
+    }
+  }
+  .item-new {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .item-new {
+    padding: 20px;
+    border-bottom: solid 1px #eee;
+  }
+  .new-title {
+    width: 100%;
+    font-weight: bold;
+    font-size: 14px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .new-content {
+    width: 100%;
+    font-size: 12px;
+    margin-top: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .w-80 {
+    width: 80%;
+  }
+}
+</style>
